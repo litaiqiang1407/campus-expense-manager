c73b9dbd520eba71c196fcfb6f8b0748dc656454
popivn <popixworkspace@gmail.com>
2024-09-30 10:27:12 +0700

    fix color

--- resources/js/Pages/Transaction/Index.vue
+++ resources/js/Pages/Transaction/Index.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="flex flex-col h-screen">
-        <!-- Header Top -->
-        <header class="h-20 px-4 py-2 flex items-center relative">
+        <!-- Header -->
+        <header class="h-20 px-4 py-2 flex flex-col items-center relative bg-white">
+            <!-- Header Top -->
             <div
                 class="balance-section flex flex-col items-center justify-center flex-1 absolute left-1/2 transform -translate-x-1/2">
                 <h2 class="balance-label text-gray-400 font-bold text-center text-xs">Balance</h2>
@@ -15,18 +16,19 @@
                     <font-awesome-icon icon="ellipsis-vertical" />
                 </span>
             </div>
+
+            <!-- Header Bottom -->
+            <div
+                class="cursor-pointer transaction-type-container bg-[#F3F3F3] flex justify-center items-center p-2 max-w-max mx-auto mt-4 rounded">
+                <!-- Sử dụng lớp rounded -->
+                <img src="/public/assets/img/wallet.jpg" alt="Wallet" class="h-6 w-6 mr-2 rounded-full" />
+                <h3 class="transaction-type text-black text-lg mr-2">Cash</h3>
+                <font-awesome-icon icon="chevron-down" />
+            </div>
         </header>
 
-        <!-- Header Bottom -->
-        <div
-            class="cursor-pointer transaction-type-container bg-gray-200 flex justify-center items-center p-2 max-w-max mx-auto">
-            <img src="/public/assets/img/wallet.jpg" alt="Wallet" class="h-6 w-6 mr-2 rounded-full" />
-            <h3 class="transaction-type text-black text-lg mr-2">Cash</h3>
-            <font-awesome-icon icon="chevron-down" />
-        </div>
-
         <!-- History Management -->
-        <div class="flex justify-between items-center mt-2 w-full max-w-md mx-auto px-4">
+        <div class="flex justify-between items-center m-0 pt-5 w-full max-w-md mx-auto px-4 bg-white">
             <span class="text-black cursor-pointer font-medium uppercase relative pb-1" @click="selectMonth('last')">
                 LAST MONTH
                 <span v-if="selectedMonth === 'last'" class="w-full h-0.5 bg-black absolute bottom-0 left-0"></span>
@@ -41,7 +43,8 @@
             </span>
         </div>
 
-        <main class="bg-gray-200 flex flex-1 items-center justify-center">
+        <!-- Main Content -->
+        <main class="bg-[#EFFBFF] flex flex-1 items-center justify-center">
             <div v-if="!hasData">
                 <NoData message="Tap + to add one" />
             </div>
@@ -51,9 +54,9 @@
         </main>
 
         <!-- Footer -->
-        <footer class="flex flex-col items-center">
+        <!-- <footer class="flex flex-col items-center">
             <span class="text-xs">Version 1</span>
-        </footer>
+        </footer> -->
     </div>
 </template>
 
@@ -66,10 +69,6 @@ const hasData = ref(false);
 const router = useRouter();
 const balance = '$0';
 const selectedMonth = ref('this');
-
-const goToSignup = () => {
-    router.push({ name: 'Signup' });
-};
 
 const selectMonth = (month) => {
     selectedMonth.value = month;
