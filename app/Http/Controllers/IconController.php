08bc553e93bbd138fb1fd158568c5406e1e55c37
Ly Thai Cuong <ltc1407.5532@gmail.com>
2024-10-09 10:48:51 +0700

    Update

--- app/Http/Controllers/IconController.php
+++ app/Http/Controllers/IconController.php
@@ -10,25 +10,22 @@ use App\Models\Icon;
 class IconController extends Controller
 {
     public function index(Request $request)
-{
-    // Retrieve all icons from the database
-    $icons = Icon::select('id', 'name', 'path')->get();
+    {
+        $icons = Icon::select('id', 'name', 'path')->get();
 
-    // Transform the paths to include the complete URL
-    $icons->transform(function ($icon) {
-        $icon->path = asset($icon->path);
-        return $icon;
-    });
+        $icons->transform(function ($icon) {
+            $icon->path = asset($icon->path);
+            return $icon;
+        });
 
-    if ($request->wantsJson()) {
-        return response()->json([
-            'icons' => $icons
+        if ($request->wantsJson()) {
+            return response()->json([
+                'icons' => $icons
+            ]);
+        }
+
+        return Inertia::render('Icon/Index', [
+            'icons' => $icons,
         ]);
     }
-
-    // Pass the data to the view
-    return Inertia::render('Icon/Index', [
-        'icons' => $icons,
-    ]);
-}
 }
--- resources/js/Pages/Icon/Index.vue
+++ resources/js/Pages/Icon/Index.vue
@@ -1,8 +1,7 @@
 <template>
     <div class="bg-white min-h-screen">
         <div class="grid grid-cols-5 gap-4 p-6">
-            <!-- Display the list of icons from the API data -->
-            <div v-for="(ic, index) in icon" :key="ic.id" class="h-full w-full rounded-full flex items-center justify-center">
+            <div v-for="(ic, index) in icon" :key="index" class="h-full w-full rounded-full flex items-center justify-center">
                 <img :src="ic.path" :alt="ic.name" class="object-cover" />
             </div>
         </div>
@@ -12,22 +11,18 @@
 
 <script setup>
 import { ref, onMounted } from 'vue';
-import axios from 'axios';
 
-const icon = ref([]); // Variable to store icon data
+const icon = ref([]); 
 
-// Function to call the API to fetch icon data
 const fetchIcon = async () => {
     try {
-        // Call the API to get data from the `Icon` route
         const response = await axios.get(route('Icon'));
-        icon.value = response.data.icons; // Assign icon data from API to the variable
+        icon.value = response.data.icons; 
     } catch (error) {
-        console.error('Error fetching icons:', error); // Catch and log errors if any occur
+        console.error('Error fetching icons:', error); 
     } 
 };
 
-// Call the fetchIcon function when the component is mounted
 onMounted(() => {
     fetchIcon();
 });
