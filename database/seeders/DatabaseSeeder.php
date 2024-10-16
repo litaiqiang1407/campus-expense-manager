f299afa46f245173ae3887e0a8023db9a6c11d9d
popivn <popixworkspace@gmail.com>
2024-10-16 09:06:22 +0700

    hehe

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+        $this->call([
+            IconsTableSeeder::class,
+            CategoriesTableSeeder::class,
+            WalletTypesTableSeeder::class,
+            WalletsTableSeeder::class,
+            TransactionSeeder::class
+        ]);
+    }
+}
