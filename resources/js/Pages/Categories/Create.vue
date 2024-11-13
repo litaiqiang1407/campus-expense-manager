2c6b22b17b191f87d1bdfe4ebb0f72693c72b605
Tran Vu Ca <tranvuca213@gmail.com>
2024-11-13 09:36:15 +0700

    Resolve merge conflicts

--- resources/js/Pages/Categories/Create.vue
+++ resources/js/Pages/Categories/Create.vue
@@ -2,9 +2,8 @@
   <div class="p-4 max-w-full overflow-hidden relative min-h-screen">
     <div class="space-y-4 mb-16">
       <div class="space-y-4 max-w-full">
-
         <div class="flex items-center">
-          <font-awesome-icon icon="circle-question" class="text-secondaryText size-10 p-3"/> 
+          <font-awesome-icon icon="circle-question" class="text-secondaryText size-10 p-3" @click="goToIconPage"/> 
           <input type="text" placeholder="Category Name" class="ml-2 flex-1 border-b border-gray-400 focus:border-black outline-none text-xl font-medium placeholder-gray-400 bg-transparent" />
         </div>
     
@@ -35,8 +34,11 @@
   </div>
 </template>
   
-<script>
-    
+<script setup>
+import { useRouter } from 'vue-router';
+const router = useRouter();
+const goToIconPage = () => {
+    router.push({ name: 'Icon' }); 
+};
 </script>
-  
   
